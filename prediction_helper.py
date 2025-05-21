f04625982ddbbb3db0748fda5da1a750356e3fb7
productankur <ankur.product.champ@gmail.com>
2025-05-22 01:25:10 +0530

    Update prediction_helper.py

--- prediction_helper.py
+++ prediction_helper.py
@@ -1,7 +1,7 @@
 # codebasics ML course: codebasics.io, all rights reserverd
 
 import pandas as pd
-from joblib import load
+import joblib
 
 model_young = joblib.load("model_young.joblib")
 model_rest = joblib.load("model_rest.joblib")
